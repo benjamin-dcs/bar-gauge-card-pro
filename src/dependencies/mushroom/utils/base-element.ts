72a72f52b05ae30dd4f6cb5a671f644083008d36
Benjamin <78026082+benjamin-dcs@users.noreply.github.com>
2026-05-25 15:38:06 +0200

    Empty

--- src/dependencies/mushroom/utils/base-element.ts
+++
@@ -1,6 +0,0 @@
-import { HomeAssistant } from "../../ha";
-
-export function computeDarkMode(hass?: HomeAssistant): boolean {
-  if (!hass) return false;
-  return (hass.themes as any).darkMode as boolean;
-}
